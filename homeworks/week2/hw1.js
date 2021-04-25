9c91287a04d4aa0c9c2fc8eb918baf3ffb403fe1
Lindsay <lliny0214tw@icloud.com>
2021-04-25 18:09:24 +0800

    week2 差第二題尚未完成

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,16 @@
+// 給定 n（1<=n<=30），依照規律「印出」正確圖形
 function printStars(n) {
-
+    if (n >= 1 && n<=30) {
+        var a = ''
+        for(var i = 1; i<=n; i++){
+            a += '*'
+        }
+        console.log(a.length)
+        console.log(a)
+    }
 }
 
-printStars(5)
+printStars(0)
+printStars(50)
+printStars(30)
+printStars(32)
--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,23 @@
-function capitalize(str) {
-  
-}
+// 給定一字串，把第一個字轉成大寫之後「回傳」，若第一個字不是英文字母則忽略。
 
-console.log(capitalize('hello'));
+// capitalize('nick')
+// 正確回傳值：Nick
+
+// capitalize('Nick')
+// 正確回傳值：Nick
+
+// capitalize(',hello')
+// 正確回傳值：,hello
+
+
+function capitalize(str) {
+    var n = str[0]
+    if (n >='A' && n<='Z') {
+        var str = n.toUpperCase()
+        return n
+        }
+    }
+
+// console.log(capitalize('hello'));
+capitalize('nick')
+
