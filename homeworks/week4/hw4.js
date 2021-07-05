7226e24808b7b3a4e923c284e33431b0821f5205
Lindsay <lliny0214tw@icloud.com>
2021-07-05 15:57:15 +0800

    week11_1

--- homeworks/week4/hw4.js
+++ homeworks/week4/hw4.js
@@ -1,24 +1,24 @@
-const request = require('request');
+// eslint-disable-next-line import/no-unresolved
+const request = require('request')
 
-const CLIENT_ID = '1hq0ejhijx89ngqykc5opyc46k11c7';
-const API_ENDPOINT = 'https://api.twitch.tv/kraken';
-
+const CLIENT_ID = '1hq0ejhijx89ngqykc5opyc46k11c7'
+const API_ENDPOINT = 'https://api.twitch.tv/kraken'
 
 request({
   method: 'GET',
   url: `${API_ENDPOINT}/games/top`,
   headers: {
     'Client-ID': CLIENT_ID,
-    'Accept': 'application/vnd.twitchtv.v5+json'
+    Accept: 'application/vnd.twitchtv.v5+json'
   }
-}, function(err, res, body) {
+}, (err, res, body) => {
   if (err) {
     return console.log(err)
   }
 
   const data = JSON.parse(body)
   const games = data.top
-  for(let game of games) {
-    console.log(game.viewers + ' ' + game.game.name)
+  for (const game of games) {
+    console.log(`${game.viewers} ${game.game.name}`)
   }
 })
